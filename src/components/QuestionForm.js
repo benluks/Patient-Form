f766df257275f9caa9989ecede4a77ad51d20c3b
benluks <70067243+benluks@users.noreply.github.com>
2021-02-28 13:45:01 -0500

    isolate bool from other questions

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -2,53 +2,53 @@ import React from 'react';
 import questionnaire from '../data/questions';
 import { Button, DatePicker, Form, InputNumber, Select, Row, Col } from 'antd';
 import 'antd/dist/antd.css';
+import BoolQuestion from './question/BoolQuestion';
 
 const { Option } = Select;
 
 function QuestionForm() {
   const [form] = Form.useForm();
 
-  console.log(form);
   return (
     <Row justify='center'>
       <Col span={12}>
-        <Form layout='vertical' name='basic'>
-          {questionnaire.map((quest, index) => (
-            <Form.Item
-              label={quest.question}
-              name={quest.title}
-              rules={[
-                {
-                  required: quest.isMandatory,
-                  message: 'Please enter your answer here',
-                },
-              ]}
-              key={index}
-            >
-              {quest.dataType === 'int' && <InputNumber min={0} />}
-              {quest.dataType === 'date' && <DatePicker />}
-              {quest.dataType === 'bool' && (
-                <Select placeholder='Ja oder nein bitte wählen'>
-                  <Option value={true}>Ja</Option>
-                  <Option value={false}>Nein</Option>
-                </Select>
-              )}
-              {quest.dropdown && (
-                <Select
-                  placeholder={`${quest.title} bitte wählen`}
-                  onChange={(value) =>
-                    form.setFieldsValue({ [quest.title]: value })
-                  }
-                >
-                  {quest.dropdown.map((opt, index) => (
-                    <Option value={opt} key={index}>
-                      {opt}
-                    </Option>
-                  ))}
-                </Select>
-              )}
-            </Form.Item>
-          ))}
+        <Form form={form} layout='vertical' name='basic'>
+          {questionnaire.map((quest, index) =>
+            quest.dataType === 'bool' ? (
+              <BoolQuestion
+                key={index}
+                question={quest}
+                form={form}
+                handleChange={(value) => {
+                  form.setFieldsValue({ [quest.title]: value });
+                  console.log(form.getFieldValue(quest.title));
+                }}
+              />
+            ) : (
+              <Form.Item
+                label={quest.question}
+                rules={[
+                  {
+                    required: quest.isMandatory,
+                    message: 'Please enter your answer here',
+                  },
+                ]}
+                key={index}
+              >
+                {quest.dataType === 'int' && <InputNumber min={0} />}
+                {quest.dataType === 'date' && <DatePicker />}
+                {quest.dropdown && (
+                  <Select placeholder={`${quest.title} bitte wählen`}>
+                    {quest.dropdown.map((opt, index) => (
+                      <Option value={opt} key={index}>
+                        {opt}
+                      </Option>
+                    ))}
+                  </Select>
+                )}
+              </Form.Item>
+            )
+          )}
           <Button type='primary' htmlType='submit'>
             Submit
           </Button>
--- src/components/question/BoolQuestion.js
+++ src/components/question/BoolQuestion.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import { Form, Select } from 'antd';
+import { Form, Input, Select } from 'antd';
 
-function BoolQuestion() {
+function BoolQuestion({ question, form, handleChange }) {
   const { Option } = Select;
 
-  const [form] = Form.useForm();
-
-  return <div></div>;
+  return (
+    <>
+      <Form.Item
+        label={question.question}
+        rules={[
+          {
+            required: question.isMandatory,
+            message: 'Please enter your answer here',
+          },
+        ]}
+      >
+        <Select onChange={handleChange} placeholder='Bitte ja oder nein wählen'>
+          <Option value={true}>Ja</Option>
+          <Option value={false}>Nein</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item
+        noStyle
+        shouldUpdate={(prevValues, currentValues) =>
+          prevValues[question.title] !== currentValues[question.title]
+        }
+      >
+        {({ getFieldValue }) =>
+          getFieldValue(question.title) ? (
+            <Form.Item
+              label={question.ifTrueQuestion}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please enter your answer here',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          ) : null
+        }
+      </Form.Item>
+    </>
+  );
 }
 
 export default BoolQuestion;
