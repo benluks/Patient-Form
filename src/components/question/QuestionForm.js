ca876223ff86d23a42c0cae356e649660b64c5ca
Yannick Börner <ylboerner@gmail.com>
2021-02-28 17:31:27 +0000

    Fix horizontal spacing

--- src/components/question/QuestionForm.js
+++ src/components/question/QuestionForm.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import questionnaire from '../../data/questions';
-import { Button, DatePicker, Form, Input } from 'antd';
+import { Button, DatePicker, Form, Input, Row, Col } from 'antd';
 import 'antd/dist/antd.css';
 
 function QuestionForm() {
   return (
-    <Form layout='vertical' name='basic'>
-      {questionnaire.map((quest, index) => (
-        <Form.Item
-          label={quest.question}
-          name={quest.title}
-          rules={[
-            {
-              required: quest.isMandatory,
-              message: 'Please enter your answer here',
-            },
-          ]}
-          key={index}
-        >
-          {quest.dataType === 'int' && <Input />}
-          {quest.dataType === 'date' && <DatePicker />}
-        </Form.Item>
-      ))}
-      <Button type='primary' htmlType='submit'>
-        Submit
+    <Row justify="center">
+      <Col span={12}>
+        <Form layout='vertical' name='basic'>
+          {questionnaire.map((quest, index) => (
+            <Form.Item
+              label={quest.question}
+              name={quest.title}
+              rules={[
+                {
+                  required: quest.isMandatory,
+                  message: 'Please enter your answer here',
+                },
+              ]}
+              key={index}
+            >
+              {quest.dataType === 'int' && <Input />}
+              {quest.dataType === 'date' && <DatePicker />}
+            </Form.Item>
+          ))}
+          <Button type='primary' htmlType='submit'>
+            Submit
       </Button>
-    </Form>
+        </Form>
+      </Col>
+    </Row>
   );
 }
 
