fc551aa6c2a6b17f4e2c6a69cd7604315df2ffca
Yannick Börner <ylboerner@gmail.com>
2021-02-28 21:51:23 +0000

    Add logo

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Typography, Row, Col } from 'antd';
+import { Image, Layout, Typography, Row, Col, Divider } from 'antd';
 import 'antd/dist/antd.css';
 
 import chariteLogo from './charite.png';
@@ -16,6 +16,9 @@ function QuestionForm() {
         <Content>
           <Row justify='center'>
             <Col span={12}>
+              <Divider></Divider>
+            <Image src={chariteLogo}></Image>
+            <Divider></Divider>
               <Questions />
             </Col>
           </Row>
